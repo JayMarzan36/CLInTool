3d4cc7c9bc3924ff93523f82722726fc60e6fc76
JayMarzan36 <jaymarzan36@gmail.com>
2025-03-10 14:20:58 -0600

    Scripting programs work, and will auto detect OS

--- project/src/CLInTool/main.py
+++ project/src/CLInTool/main.py
@@ -1,6 +1,7 @@
 import argparse
 
-from .projectGen import projectGen 
+from .projectGen import projectGen
+from .scripting import scriptGen, makeGlobal
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="CLI Tool")
@@ -14,6 +15,15 @@ def main() -> None:
     projectGenParser.add_argument("--git", help="Initialize git repository", action="store_true")
     projectGenParser.add_argument("--venv", help="Create virtual environment", action="store_true")
     
+    # scriptGen
+    scriptGenParser = subparser.add_parser("scriptGen")
+    scriptGenParser.add_argument("-n", "--name", help="Script name")
+    
+    
+    # makeGlobal
+    makeGlobalParser = subparser.add_parser("makeGlobal")
+    makeGlobalParser.add_argument("-n", "--name", help="Script name")
+    
     
     
     
@@ -23,6 +33,16 @@ def main() -> None:
     
     if args.command == "projectGen":
         projectGen.main()
+    elif args.command == "scriptGen":
+        if args.name:
+            scriptGen.scriptGen(args.name)
+        else:
+            scriptGen.interactiveMode()
+    elif args.command == "makeGlobal":
+        if args.name:
+            makeGlobal.makeScriptGlobal(args.name)
+        else:
+            makeGlobal.interactiveMode()
     else:
         parser.print_help()
         
